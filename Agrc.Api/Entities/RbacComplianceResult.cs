f6de348379d3fa89ec79fd86e2937c0339f59e2f
prakash6041 <105364622+prakash6041@users.noreply.github.com>
2025-12-27 14:12:44 +0530

    initial commit

--- Agrc.Api/Entities/RbacComplianceResult.cs
+++ Agrc.Api/Entities/RbacComplianceResult.cs
@@ -4,7 +4,7 @@
     {
         public bool Pass { get; set; }
         public List<string> Roles { get; set; } = new List<string>();
-        public int AdminCount { get; set; }
+        public int AdminCount { get; set; } = 0;
         public DateTime EvidenceGeneratedAt { get; set; }
         public object Assignments { get; set; } = default!;
     }
